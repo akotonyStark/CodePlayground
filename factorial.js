91b4ba7d565979474db7cee4b9113b118a003dba
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-10 17:35:40 +0000

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,13 @@
+let factorial = (n) => {
+  let fact = 1
+    if(n <= 1){
+        return fact
+    }
+    else{
+      for(let i = 1; i<=n; i++){
+         fact = fact * i
+      }
+      return fact
+    }
+    //return n*(n-1)
+}
