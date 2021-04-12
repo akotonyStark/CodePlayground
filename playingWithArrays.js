79cbfe180ce38a9122f384e258c1fdcd4e3a9e39
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-12 13:03:14 +0000

    Update playingWithArrays.js

--- playingWithArrays.js
+++ playingWithArrays.js
@@ -10,3 +10,13 @@ for(let i = 0; i<arr.length; i++){
   }
 }
 
+//doing an inside loop to compare elements starting from front
+console.log("---")
+for(let i = 0; i<arr.length; i++){
+  for(let j = 0; j<arr.length; j++){
+    console.log(arr[i], nums[j])
+    if(arr[i] === nums[j]){
+      console.log(arr[i])
+    }
+  }
+}
