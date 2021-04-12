4d690d6ba18b860fceb992818d38639ed2ad467d
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-12 13:19:18 +0000

    Update playingWithArrays.js

--- playingWithArrays.js
+++ playingWithArrays.js
@@ -1,19 +1,26 @@
-let arr = [1,2]
-let nums = [2,1]
+let arr = [1,2,3]
+let nums = [1,2,3]
 
 
-//looping through side by side
 for(let i = 0; i<arr.length; i++){
   console.log(arr[i], nums[i])
   if(arr[i] === nums[i]){
     console.log(arr[i])
   }
 }
-
-//doing an inside loop to compare elements starting from front
 console.log("---")
 for(let i = 0; i<arr.length; i++){
-  for(let j = 0; j<arr.length; j++){
+  for(let j = 0; j<nums.length; j++){
+    console.log(arr[i], nums[j])
+    if(arr[i] === nums[j]){
+      console.log(arr[i])
+    }
+  }
+}
+console.log("---")
+
+for(let i = 0; i<arr.length; i++){
+  for(let j = nums.length-1; j>=0; j--){
     console.log(arr[i], nums[j])
     if(arr[i] === nums[j]){
       console.log(arr[i])
@@ -21,9 +28,11 @@ for(let i = 0; i<arr.length; i++){
   }
 }
 
-//looping through inner loop from the tail and outerloop from the head
+console.log("---")
+
+//traverse through and do not start from beginning after each loop
 for(let i = 0; i<arr.length; i++){
-  for(let j = arr.length-1; j>=0; j--){
+  for(let j = i; j<nums.length; j++){
     console.log(arr[i], nums[j])
     if(arr[i] === nums[j]){
       console.log(arr[i])
