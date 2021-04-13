1e624b52be415122c78cf9d7381b2eff23f7eca9
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-13 07:36:21 +0000

    Update compareArrays.js

--- compareArrays.js
+++ compareArrays.js
@@ -1,4 +1,5 @@
 function compareArrays(arr1,arr2){
+  //without stringifying them, these are two separate array objects
   if(JSON.stringify(arr1) === JSON.stringify(arr2)){
     console.log(true)
   }
