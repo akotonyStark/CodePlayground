f3cda6b8f74266d0e610cb719a897b280f46f1cf
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-18 16:24:39 +0000

    Update DictionariesUsingMaps.js

--- DictionariesUsingMaps.js
+++ DictionariesUsingMaps.js
@@ -1,7 +1,8 @@
 //creating a russian dictionary
 
 //objects to be passed to the Map
-let five = {name: "five"}, four = {name:"four"}
+let five = {name: "five"}, four = {name:"four"},
+ one = {name:"one"}, two = {name: "two"}
 
 //can also pass iterables to the map like this
 let dictionary = new Map([[one,"adin"], [two,"dva"]]);
@@ -14,5 +15,7 @@ dictionary.set(five,"Piat");
 console.log(dictionary.get(three))
 console.log(dictionary.get(one))
 console.log(dictionary.get(two))
+console.log(dictionary.get(four))
+console.log(dictionary.get(five))
 
 console.log(dictionary.size)
