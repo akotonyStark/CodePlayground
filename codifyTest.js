39da1eb84d62b7239b5a36280e746fcf0838b91a
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-05-10 18:23:30 +0000

    Update codifyTest.js

--- codifyTest.js
+++ codifyTest.js
@@ -1,52 +1,24 @@
+
+let arr =   [1, 3, 5,8,6, 4, 1, 2]
+//let arr = [1]
+//let arr =   [1,2,3]
+//let arr =   [-1, -3]
+//let arr = [1,2,40000]
+//let arr = [-1000000, 1000000]
+
+
 function solution(arr){
-arr.sort((a,b) => a-b)
+  arr.sort((a,b) => a-b)
+  arr = [...new Set(arr)]
+  console.log(arr)
 
-function checkMin(num){
-  let min = arr[0]
-  return num>min
-}
-
-  let i =0
-  let nextMin = arr.find(checkMin)
-  let nextNextMin = nextMin + 1
-  
-  if(arr.length == 1 && arr[0] == 1){
-  	return 2
+  let item = 0
+  while(arr.includes(item + 1)){
+    item ++
   }
-  else if((arr.length == 1 && arr[0] <= 0) || arr.length == 0){
-   return 1
-  }
-  else if(arr.length == 1 && arr[0] >= 1 ){
-   return arr[0] - 1
-  }
-  else{
-    while(i < arr.length){
-     if((arr[i] >= nextMin) && (!arr.includes(nextNextMin))){
-     console.log('a')
-      return (nextNextMin)
-    }
-    else if(arr[i] < 0){
-     return (1)
-    }
-    else{
-      arr = arr.splice(arr.indexOf(nextMin))     
-      nextNextMin = arr[0] + 1
-      //console.log(nextNextMin)
-      if((arr[i] >= nextMin) && (!arr.includes(nextNextMin))){
-      return (nextNextMin) 
-      break
-      } 
-      else{
-      nextMin = arr.find(checkMin)
-      //console.log(arr)
-     	if((arr[i] >= nextMin) && (!arr.includes(nextMin + 1))){
-     	      // console.log('a')
-     	      return (nextMin + 1)
-     	      } 
-    } i++
-  }
-}
+  return item + 1
   }
    
 
-}
+console.log(solution(arr))
+
