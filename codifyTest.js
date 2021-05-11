d17ceb73976ab18c76c0e52c8544baac8faea3cd
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-05-11 14:38:03 +0000

    Update codifyTest.js

--- codifyTest.js
+++ codifyTest.js
@@ -22,3 +22,45 @@ function solution(arr){
 
 console.log(solution(arr))
 
+
+// optmized solution above
+let arr =   [1,2,40000]//let arr = [1]//let arr =   [1,2,3] //let arr =   [-1, -3] //let arr = [1,2,40000] //let arr = [-1000000, 1000000]
+
+let solution = (arr) => {
+
+if(arr.length < 2 || (arr.length == 2 && arr[0] < 0)){
+	return 1
+}
+
+else{
+ arr.sort((a,b) => a-b)
+  arr = [...new Set(arr)]
+  let arrCopy = [...new Set(arr)]
+  let start= 0, new_start = 0
+  
+  let midpoint = (arr.length)/2
+  let left = arr.splice(0,midpoint)
+  let right = arrCopy.splice(midpoint)
+  
+  do{
+  	if(left.includes(start + 1)){
+    	//move to the right array
+    }
+    else{
+    	new_start = left[left.length-1]
+      return checkAgain(right, new_start)   
+    } 
+    start++
+  }while(start < arr.length)
+}
+ 
+}
+  
+  const checkAgain = (data, new_start) => {
+    while(data.includes(new_start + 1)){
+      new_start++
+    }
+    return new_start + 1
+  }
+  
+console.log(solution(arr))
