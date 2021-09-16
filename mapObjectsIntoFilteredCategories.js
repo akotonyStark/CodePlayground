425eb1407ccf74ced1ec01ea9c69fc74c43166d7
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-09-16 13:02:29 +0000

    Create mapObjectsIntoFilteredCategories.js

---
+++ mapObjectsIntoFilteredCategories.js
@@ -0,0 +1,8 @@
+let data = [
+   {name: 'Nala', type: 'dog'},
+   {name: 'George', type: 'monkey'},
+   {name: 'Scar', type: 'dog'},
+   {name: 'Nemo', type: 'fish'},
+   {name: 'Tasha', type: 'dog'},
+   {name: 'Leo', type: 'cat'}
+  ]
