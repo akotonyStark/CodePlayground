203846139726ef1f6ebdbf4a28bb314ca0bfc509
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-09-16 16:04:14 +0000

    Update mapObjectsIntoFilteredCategories.js

--- mapObjectsIntoFilteredCategories.js
+++ mapObjectsIntoFilteredCategories.js
@@ -6,3 +6,17 @@ let data = [
    {name: 'Tasha', type: 'dog'},
    {name: 'Leo', type: 'cat'}
   ]
+
+const categories = []
+let newObj = {}
+
+data.forEach(animal => {
+   categories.push(animal.type)
+})
+
+const uniqueCategories = [...new Set(categories)]
+
+uniqueCategories.forEach(category => {
+   newObj[category] = data.filter(animal => animal.type === category)
+})
+console.log(newObj)
