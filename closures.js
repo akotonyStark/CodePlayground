2adabc0ca510102a339fd3c491ceaa5683a42f4f
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-09-30 15:28:06 +0000

    Create closures.js

---
+++ closures.js
@@ -0,0 +1,9 @@
+const myFunction = () => {
+  const message = 'This is my message'
+  const printMessage = () => {
+    console.log(message)
+  }
+  printMessage()
+}
+
+myFunction() //prints - this is my message
