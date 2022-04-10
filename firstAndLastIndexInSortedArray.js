87f4c263187ea4f3fa675dc06492539c43d87c45
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-10 00:05:14 +0000

    Create firstAndLastIndexInSortedArray.js

---
+++ firstAndLastIndexInSortedArray.js
@@ -0,0 +1,15 @@
+let arr = [2,4,5,5,5,5,5,7,9,9]
+let target = 5
+let output = []
+for(let i=0; i<arr.length; i++){
+  if(arr[i] === target && arr[i-1] != target){
+    output.push(i)
+    continue;
+  }
+  else if(arr[i] === target && arr[i+1] != target){
+    output.push(i)
+  }
+    
+  
+}
+console.log(output)
