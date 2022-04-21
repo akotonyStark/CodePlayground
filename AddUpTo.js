0c8d9d3b6026d95385c64a1ab2542a46745c8d62
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-21 12:05:36 +0000

    check for performance runtime

--- AddUpTo.js
+++ AddUpTo.js
@@ -12,5 +12,10 @@ function addUp(n){
     return n * (n+1)/2
 }
 
-console.log(addUpTo(100))
-console.log(addUp(100))
+let t1 = performance.now();
+// console.log(addUpTo(1000000))
+
+console.log(addUp(1000000))
+let t2 = performance.now()
+
+console.log((t2-t1)/1000)
