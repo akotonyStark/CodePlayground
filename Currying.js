f023fe1e7ae4aec75437d32af89d447f530726ef
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-08-22 18:00:50 +0000

    Update Currying.js

--- Currying.js
+++ Currying.js
@@ -16,3 +16,8 @@ const curryFn = (fn) => {
 
 const curriedSum = curryFn(sum)
 console.log(curriedSum(2)(3)(5))
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+const add5 = add3(5)
+console.log(add5)
