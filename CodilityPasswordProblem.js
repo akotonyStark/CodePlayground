8f89894b615870176557e647b4f30b792dac45dc
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2024-05-07 17:53:15 -0400

    Create CodilityPasswordProblem.js
    You would like to set a password for a bank account. However, there are three restrictions on the format of the password:
    
    it has to contain only alphanumerical characters (a−z, A−Z, 0−9);
    there should be an even number of letters;
    there should be an odd number of digits.
    You are given a string S consisting of N characters. String S can be divided into words by splitting it at, and removing, the spaces. The goal is to choose the longest word that is a valid password. You can assume that if there are K spaces in string S then there are exactly K + 1 words.
    
    For example, given "test 5 a0A pass007 ?xy1", there are five words and three of them are valid passwords: "5", "a0A" and "pass007". Thus the longest password is "pass007" and its length is 7. Note that neither "test" nor "?xy1" is a valid password, because "?" is not an alphanumerical character and "test" contains an even number of digits (zero).
    
    Write a function:
    
    function solution(S);
    
    that, given a non-empty string S consisting of N characters, returns the length of the longest word from the string that is a valid password. If there is no such word, your function should return −1.
    
    For example, given S = "test 5 a0A pass007 ?xy1", your function should return 7, as explained above.
    
    Assume that:
    
    N is an integer within the range [1..200];
    string S consists only of printable ASCII characters and spaces.
    In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.

---
+++ CodilityPasswordProblem.js
@@ -0,0 +1,65 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function isAlphaNumeric(str) {
+    // Regex to check valid
+    // alphanumeric string  
+    let regex = new RegExp(/^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$/);
+ 
+    // if str 
+    // is empty return false
+    if (str == null) {
+        return "false";
+    }
+ 
+    // Return true if the str
+    // matched the ReGex
+    if (regex.test(str) == true) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
+
+function hasEvenNumberOfLetters(str){
+    let letters = str.replace(/[^a-zA-Z]/g, '');
+
+    if(letters.length % 2 == 0){
+        return true
+    }
+    return false
+}
+
+function hasOddNuberOfDigits(str){
+    let numbers = str.replace(/[^0-9]/g,"");
+    if(numbers.length % 2 != 0){
+        return true
+    }
+    return false
+}
+function solution(S) {
+    // Implement your solution 
+    let words = S.split(' ')
+    let validPasswords = []
+    for(let i=0; i<words.length; i++){
+        if(isAlphaNumeric(words[i]) && hasEvenNumberOfLetters(words[i]) && hasOddNuberOfDigits(words[i])){
+            validPasswords.push(words[i])
+        }
+    }
+    //console.log("Valid", validPasswords)
+    //let arr = [ '5', 'a0A', 'pass007', '?xy1' ]
+
+        let lengths = []
+        for(let i=0; i<validPasswords.length; i++){
+            lengths.push(validPasswords[i].length)
+        }
+
+        //console.log(lengths)
+        let maxLength = Math.max(...lengths)
+        //console.log(lengths.indexOf(maxLength))
+
+        let longestPass = validPasswords[lengths.indexOf(maxLength)]
+        //console.log(longestPass)
+        return maxLength
+}
